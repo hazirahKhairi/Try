fe91f1efd482f2d862125c7de4c60568481512d4
DELL <DELL@DESKTOP-O79BP7D>
2019-01-07 06:34:04 +0800

    file

---
+++ InterfaceDotnet/InterfaceDotnet/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterfaceDotnet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+        public ActionResult Fried()
+        {
+            ViewBag.Message = "Choose your menu.";
+
+            return View();
+        }
+
+        public ActionResult Mee()
+        {
+            ViewBag.Message = "Choose your menu.";
+
+            return View();
+        }
+
+        public ActionResult Soup()
+        {
+            ViewBag.Message = "Choose your menu.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Share your feedback by contacting us here";
+            return View();
+        }
+    }
+}
---
+++ InterfaceDotnet/InterfaceDotnet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InterfaceDotnet.MvcApplication" Language="C#" %>
